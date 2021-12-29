78a26ec42fb7f46aafe12e8828e3ca455c5fa7c6
KyubinHwang <choco1drink@gmail.com>
2021-12-29 17:28:35 +0900

    splash screen 및 메인화면 조금 수정

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -8,42 +8,56 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  int _selectedIndex = 0;
-
-  List _pages = [
-    Text('TimeLine'),
-    Text('Diary'),
-    Text('Report'),
-    Text('MyPage'),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: _pages[_selectedIndex]),
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          fixedColor: Colors.black,
-          unselectedItemColor: Colors.grey.withOpacity(.50),
-          onTap: _onItemTapped,
-          currentIndex: _selectedIndex,
-          items: <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.access_time_outlined),
-                title: Text('TimeLine')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.book_outlined), title: Text('Diary')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.bar_chart_outlined), title: Text('Report')),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle_outlined),
-                title: Text('MyPage')),
-          ]),
-    );
+        appBar: _buildAppBar(),
+        body: Stack(
+          children: [
+            Image.asset('images/splash.png'),
+            // Container(
+            //   color: Colors.purple[100],
+            // ),
+            DraggableScrollableSheet(
+              builder:
+                  (BuildContext context, ScrollController scrollController) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: ListView.builder(
+                    controller: scrollController,
+                    itemCount: 10,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(title: Text('Item $index'));
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ));
   }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        "   ONDO",
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.purple[100],
+      elevation: 0.0,
+      actions: <Widget>[
+        IconButton(
+          onPressed: () {},
+          icon: Image.asset('images/report_icon.png'),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Image.asset('images/alarm_icon.png'),
+        ),
+      ],
+    );
   }
 }
