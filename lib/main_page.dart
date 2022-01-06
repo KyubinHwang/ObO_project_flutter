41590527e68aebaaaed481c3156b372c03bf31de
KyubinHwang <choco1drink@gmail.com>
2022-01-06 22:46:25 +0900

    주간 view 추가하고 tab부분 좀 수정

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sliding_sheet/sliding_sheet.dart';
+import 'package:date_picker_timeline/date_picker_timeline.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
@@ -9,6 +11,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  AssetImage switching = const AssetImage('./assets/timeline_icon.png');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,57 +51,159 @@ class _MainPageState extends State<MainPage> {
                 ]),
           ),
           SlidingSheet(
+            minHeight: 500,
             elevation: 5,
             cornerRadius: 16,
             builder: (context, state) {
-              return SizedBox(
-                  height: 750,
-                  child: Center(
-                      child: DefaultTabController(
-                    length: 2,
-                    child: Scaffold(
-                      appBar: AppBar(
-                        backgroundColor: Colors.white,
-                        bottom: const TabBar(
-                          tabs: [
-                            Tab(
-                              icon: Image(
+              return Column(
+                children: [
+                  DatePicker(
+                    DateTime.now(),
+                    height: 100,
+                    width: 80,
+                    initialSelectedDate: DateTime.now(),
+                    selectionColor: Colors.black,
+                    selectedTextColor: Colors.white,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                changing(const AssetImage(
+                                  './assets/timeline_icon.png',
+                                ));
+                              },
+                              child: const Image(
                                   width: 35,
                                   image: AssetImage(
                                     './assets/timeline_icon.png',
                                   )),
                             ),
-                            Tab(
-                              icon: Image(
+                            const SizedBox(
+                              height: 10,
+                              child: DecoratedBox(
+                                decoration: BoxDecoration(color: Colors.red),
+                              ),
+                            ),
+                            Container(
+                              height: 4,
+                              width: 80,
+                              decoration: (switching ==
+                                      const AssetImage(
+                                          './assets/timeline_icon.png'))
+                                  ? const BoxDecoration(
+                                      gradient: LinearGradient(
+                                      colors: [
+                                        Color(0xff3B4383),
+                                        Color(0xff5D4F83),
+                                        Color(0xffA67E90),
+                                      ],
+                                      begin: Alignment.centerRight,
+                                      end: Alignment.centerLeft,
+                                    ))
+                                  : const BoxDecoration(
+                                      color: Colors.transparent),
+                            )
+                          ]),
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            InkWell(
+                              onTap: () {
+                                changing(const AssetImage(
+                                  './assets/diary_icon.png',
+                                ));
+                              },
+                              child: const Image(
                                   width: 35,
-                                  image: AssetImage('./assets/diary_icon.png')),
+                                  image: AssetImage(
+                                    './assets/diary_icon.png',
+                                  )),
                             ),
-                          ],
-                        ),
-                      ),
-                      body: Center(
-                          child: TabBarView(children: <Widget>[
-                        Tab(
-                          child: SingleChildScrollView(
-                            child: Column(
-                              children: const [
-                                Text("타임라인"),
-                              ],
+                            const SizedBox(
+                              height: 10,
+                              child: DecoratedBox(
+                                decoration: BoxDecoration(color: Colors.red),
+                              ),
                             ),
-                          ),
-                        ),
-                        Tab(
-                          child: SingleChildScrollView(
-                            child: Column(
-                              children: const [
-                                Text("타임라인"),
-                              ],
+                            Container(
+                              height: 4,
+                              width: 80,
+                              decoration: (switching ==
+                                      const AssetImage(
+                                          './assets/diary_icon.png'))
+                                  ? const BoxDecoration(
+                                      gradient: LinearGradient(
+                                      colors: [
+                                        Color(0xff3B4383),
+                                        Color(0xff5D4F83),
+                                        Color(0xffA67E90),
+                                      ],
+                                      begin: Alignment.centerRight,
+                                      end: Alignment.centerLeft,
+                                    ))
+                                  : const BoxDecoration(
+                                      color: Colors.transparent),
                             ),
-                          ),
-                        ),
-                      ])),
-                    ),
-                  )));
+                          ]),
+                    ],
+                  ),
+                  // SizedBox(
+                  //     height: 650,
+                  //     child: Center(
+                  //         child: DefaultTabController(
+                  //       length: 2,
+                  //       child: Scaffold(
+                  //         resizeToAvoidBottomInset: false,
+                  //         appBar: AppBar(
+                  //           backgroundColor: Colors.white,
+                  //           bottom: const TabBar(
+                  //             tabs: [
+                  //               Tab(
+                  //                 icon: Image(
+                  //                     width: 35,
+                  //                     image: AssetImage(
+                  //                       './assets/timeline_icon.png',
+                  //                     )),
+                  //               ),
+                  //               Tab(
+                  //                 icon: Image(
+                  //                     width: 35,
+                  //                     image: AssetImage(
+                  //                         './assets/diary_icon.png')),
+                  //               ),
+                  //             ],
+                  //           ),
+                  //         ),
+                  //         body: Center(
+                  //             child: TabBarView(children: <Widget>[
+                  //           Tab(
+                  //             child: SingleChildScrollView(
+                  //               child: Column(
+                  //                 children: const [
+                  //                   Text("타임라인"),
+                  //                 ],
+                  //               ),
+                  //             ),
+                  //           ),
+                  //           Tab(
+                  //             child: SingleChildScrollView(
+                  //               child: Column(
+                  //                 children: const [
+                  //                   Text("타임라인"),
+                  //                 ],
+                  //               ),
+                  //             ),
+                  //           ),
+                  //         ])),
+                  //       ),
+                  //     ))),
+                ],
+              );
             },
           ),
         ],
@@ -126,5 +232,10 @@ class _MainPageState extends State<MainPage> {
         ),
       ],
     );
+  }
+
+  changing(AssetImage switchingImage) {
+    switching = switchingImage;
+    setState(() {});
   }
 }
